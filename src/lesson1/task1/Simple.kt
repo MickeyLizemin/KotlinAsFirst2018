17650afa2c375e6c103dee3be88a5d81365787a0
MickeyLizemin <1194538930@qq.com>
2018-09-18 11:23:18 +0300

    first lesson

--- src/lesson1/task1/Simple.kt
+++ src/lesson1/task1/Simple.kt
@@ -59,7 +59,8 @@ fun main(args: Array<String>) {
  * Пользователь задает время в часах, минутах и секундах, например, 8:20:35.
  * Рассчитать время в секундах, прошедшее с начала суток (30035 в данном случае).
  */
-fun seconds(hours: Int, minutes: Int, seconds: Int): Int =hours*3600+minutes*60+seconds
+fun seconds(hours: Int, minutes: Int, seconds: Int):
+        Int =hours*3600+minutes*60+seconds
 
 /**
  * Тривиальная
@@ -77,7 +78,8 @@ fun lengthInMeters(sagenes: Int, arshins: Int, vershoks: Int):
  * Пользователь задает угол в градусах, минутах и секундах (например, 36 градусов 14 минут 35 секунд).
  * Вывести значение того же угла в радианах (например, 0.63256).
  */
-fun angleInRadian(grad: Int, min: Int, sec: Int): Double = (grad + min / 60.0 + sec / 3600.0) * Math.PI / 180
+fun angleInRadian(grad: Int, min: Int, sec: Int):
+        Double = (grad + min / 60.0 + sec / 3600.0) * Math.PI / 180
 
 /**
  * Тривиальная
@@ -85,7 +87,8 @@ fun angleInRadian(grad: Int, min: Int, sec: Int): Double = (grad + min / 60.0 + 
  * Найти длину отрезка, соединяющего точки на плоскости с координатами (x1, y1) и (x2, y2).
  * Например, расстояние между (3, 0) и (0, 4) равно 5
  */
-fun trackLength(x1: Double, y1: Double, x2: Double, y2: Double): Double = sqrt(sqr(x1-x2) +sqr(y1-y2))
+fun trackLength(x1: Double, y1: Double, x2: Double, y2: Double):
+        Double = sqrt(sqr(x1-x2) +sqr(y1-y2))
 
 /**
  * Простая
@@ -113,8 +116,8 @@ fun travelMinutes(hoursDepart: Int, minutesDepart: Int, hoursArrive: Int, minute
  * Сколько денег будет на счету через 3 года (с учётом сложных процентов)?
  * Например, 100 рублей под 10% годовых превратятся в 133.1 рубля
  */
-fun accountInThreeYears(initial: Int, percent: Int): Double =
-        initial * Math.pow(1+(percent/100.0),3.0)
+fun accountInThreeYears(initial: Int, percent: Int):
+        Double = initial * Math.pow(1+(percent/100.0),3.0)
 
 /**
  * Простая
@@ -122,8 +125,8 @@ fun accountInThreeYears(initial: Int, percent: Int): Double =
  * Пользователь задает целое трехзначное число (например, 478).
  * Необходимо вывести число, полученное из заданного перестановкой цифр в обратном порядке (например, 874).
  */
-fun numberRevert(number: Int)=
-        number%10*100+(number%100/10)*10+number/100
+fun numberRevert(number: Int)
+        = number%10*100+(number%100/10)*10+number/100
 
 
 
