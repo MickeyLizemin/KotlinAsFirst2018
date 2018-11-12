c50aa240a5cbac03c41f1d9c15bd9a09b2e82eb3
MickeyLizemin <1194538930@qq.com>
2018-11-12 11:34:51 +0300

    first lesson

--- src/lesson3/task1/Loop.kt
+++ src/lesson3/task1/Loop.kt
@@ -183,10 +183,10 @@ fun squareBetweenExists(m: Int, n: Int): Boolean {
 fun collatzSteps(x: Int): Int {
     var i = 0
     var n = x
-    while (x > 0) {
-        if (x / 2 == 0) n = x / 2
-        else n = 3 * x + 1
-        i++
+    while (n != 1) {
+        i += 1
+        if (n % 2 == 0) n = n / 2
+        else n = 3 * n + 1
     }
     return i
 }
